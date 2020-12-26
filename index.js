c527310b4459ca935390ec8a696b18d35457ff9e
Samira Fadi El Samargi <samiraelsamargi2018@gmail.com>
2020-12-26 15:05:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,5 +26,5 @@ function greet(string) {
 /* Write your implementation of displayMessage() */
 
 function displayMessage(string){
-  document.getElementById("greeting").innerText.
+  document.getElementById("greeting").innerText = string
 }
